1e58c310838415a5741c2ed3034572ee6720ee78
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-06-12 17:22:28 +0200

    Add Vector lib

--- minimalPT.cpp
+++ minimalPT.cpp
@@ -4,8 +4,65 @@
 //		g++ -o minimalPT minimalPT.cpp
 
 #include <iostream>
+#include <cmath>
 
-int main()
+//
+//	MATH FUNCTIONS
+//
+
+struct Vec3
+{
+	float x = 0,y = 0,z = 0;
+	
+	Vec3(const float&x, const float& y, const float& z)
+		:x(x),y(y),z(z)
+		{}
+	
+	Vec3 inline
+	operator+(const Vec3& other)
+	{
+		return Vec3(x + other.x, y + other.y, z + other.z);
+	}
+	
+	Vec3 inline 
+	operator-(const Vec3& other)
+	{
+		return Vec3(x - other.x, y - other.y, z - other.z);
+	}
+	
+	Vec3 inline 
+	operator-()
+	{
+		return Vec3(-x, -y, -z);
+	}
+	
+	Vec3 inline 
+	operator*(const Vec3& other)
+	{
+		return Vec3(x * other.x, y * other.y, z * other.z);
+	}
+	
+	Vec3 inline
+	operator*(const float& l)
+	{
+		return Vec3(x*l, y*l, z*l);
+	}
+	
+	float inline
+	norm()
+	{
+		return sqrtf(x*x + y*y + z*z);
+	}
+	
+	float inline
+	dot(const Vec3& other)
+	{
+		return x * other.x + y * other.y + z * other.z;
+	}
+	
+};
+
+int main(int argc, char* argv[])
 {
 	
 	return 0;
